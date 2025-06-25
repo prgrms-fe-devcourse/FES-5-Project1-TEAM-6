da8413496cc59960cd28156c935cc204554957b7
ahk0413 <rladkgusgd@gmail.com>
2025-06-25 11:26:10 +0900

    fitness log ui 작업

--- js/dom/fitnessLogTemplate.js
+++ js/dom/fitnessLogTemplate.js
@@ -5,17 +5,49 @@ export function insertFitnessLogTemplate() {
   template.innerHTML = /* html */`
     <section class="log_section">
       <h3 class="sub_tit">운동 기록 🏋️</h3>
-      <button type="button" id="logAddBtn">기록 추가 +</button>
 
-      <ul class="log_list">
-        <li>
-          <div class="log_card" tabindex="0" role="button">
-            <span class="date">25/06/24</span>
-            <span class="tit">데드리프트 참 재밌었다 하핳</span>
-            <button class="log_card_delete"><span class="blind">삭제</span></button>
-          </div>
-        </li>
-      </ul>
+      <div class="log_bar">
+        <button type="button" class="log_addbtn" id="logAddBtn">기록 추가 +</button>
+
+        <ul class="log_list">
+          <li>
+            <div class="log_doc_item">
+              <button class="arrow"><span class="blind">페이지 없음</span></button>
+              <p class="tit">제목이 길어질 경우를 대비제목이 길어질 경우를 대비제목이 길어질 경우를 대비제목이 길어질 경우를 대비제목이 길어질 경우를 대비제목이 길어질 경우를 대비제목이 길어질 경우를 대비제목이 길어질 경우를 대비제목이 길어질 경우를 대비제목이 길어질 경우를 대비</p>
+
+              <div class="btn_wrap">
+                <button class="add_btn"><span class="blind">하위 페이지 추가</span></button>
+                <button class="delete_btn"><span class="blind">해당 페이지 삭제</span></button>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="log_doc_item">
+              <button class="arrow rotate"><span class="blind">하위 페이지 있음</span></button>
+              <p class="tit">제목이 길어질 경우를 대비제목이 길어질 경우를 대비</p>
+
+              <div class="btn_wrap">
+                <button class="add_btn"><span class="blind">하위 페이지 추가</span></button>
+                <button class="delete_btn"><span class="blind">해당 페이지 삭제</span></button>
+              </div>
+            </div>
+
+            <ul class="in_depth">
+              <li>
+                <div class="log_doc_item">
+                  <button class="arrow"><span class="blind">하위 페이지 없음</span></button>
+                  <p class="tit">제목이 길어질 경우를 대비제목이 길어질 경우를 대비</p>
+
+                  <div class="btn_wrap">
+                    <button class="add_btn"><span class="blind">하위 페이지 추가</span></button>
+                    <button class="delete_btn"><span class="blind">해당 페이지 삭제</span></button>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div> 
     </section>
   `;
 
